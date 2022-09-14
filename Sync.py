32a50d764845dafa88ccb4d41229d043321b986a
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-09-14 12:22:55 +0900

    search error

--- Sync.py
+++ Sync.py
@@ -10,7 +10,7 @@ folder_name2 = 'csv_original'
 folder_name3 = 'csv_resampled'
 
 subprocess.run('rclone sync ' + raspi_folder + '/folder_name1' + ' gdrive:' + folder_name1, shell=True)
-subprocess.run('rclone sync ' + raspi_folder + '/folder_name2' + ' gdrive:' + folder_name2, shell=True)
-subprocess.run('rclone sync ' + raspi_folder + '/folder_name3' + ' gdrive:' + folder_name3, shell=True)
+# subprocess.run('rclone sync ' + raspi_folder + '/folder_name2' + ' gdrive:' + folder_name2, shell=True)
+# subprocess.run('rclone sync ' + raspi_folder + '/folder_name3' + ' gdrive:' + folder_name3, shell=True)
 
 print('finished')
