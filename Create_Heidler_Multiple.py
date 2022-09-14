cd47cce5f836a6a513cf066fb050fa5035f2c9bd
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-09-14 15:52:26 +0900

    change file_name datasize

--- Create_Heidler_Multiple.py
+++ Create_Heidler_Multiple.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 
 # set parameter(10/350)
-I = 100
-k = 0.93
+I = 100000
+k = [0.93, 0.986, 0.993]
 T1 = [10, 1, 0.25]
 T2 = [350, 200, 100]
 t = np.linspace(0, 50, 100000)
@@ -30,8 +30,8 @@ if not os.path.exists(figurepath):
 # csvpath = 'D:/Project_folder/'+'csv_original/'
 csvpath = os.getcwd() + '/csv_original/'
 
-if not os.path.exists(csvpath):
-    os.mkdir(csvpath)
+# if not os.path.exists(csvpath):
+#     os.mkdir(csvpath)
 
 fig = plt.figure(figsize=(7, 5), dpi=300)
 ax = fig.add_subplot()
@@ -40,7 +40,7 @@ ax.grid(color='k', alpha=0.5)
 
 ax.set_xlabel('time (μs)', fontsize=15)
 ax.set_ylabel('Current(kA)', fontsize=15)
-ax.set_title('Thunder Waveform4 renewal', fontsize=20)
+ax.set_title('Lightning Waveform ', fontsize=20)
 
 ax.set_xlim([0, 50])
 ax.set_ylim([0, 120])
@@ -48,20 +48,20 @@ ax.set_ylim([0, 120])
 line_color = ['k', 'b', 'r']
 
 for n in range(0, 3, 1):
-    value = cal_line(t, k, I, T1[n], T2[n])
+    value = cal_line(t, k[n], I, T1[n], T2[n])
 
-    ax.plot(t, value, color=line_color[n])
+    ax.plot(t, value/1000, color=line_color[n])
 
 # set label
 name_title = ['10/350 μs', '1/200 μs', '0.25/100 μs']
 ax.legend(name_title, loc='lower right', framealpha=1)
 
-filename = 'Heidler_3patterns_Tmax(50)'
+filename = 'Heidler_3patterns_Tmax_50'
 
 fig.savefig(figurepath + filename + '.png', dpi=300)
 
 # save CSV file
-t2 = t.T
+t2 = t.T / 1e6
 i = value.T
 
 file = np.stack([t2, i], 1)
