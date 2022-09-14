2b1ae19624b227a00ccf6345565f47661d758a7d
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-09-14 12:18:58 +0900

    add folder

--- Sync.py
+++ Sync.py
@@ -5,6 +5,12 @@ raspi_folder = os.getcwd()
 
 # print(raspi_folder)
 
-gdrive_name = 'png'
+folder_name1 = 'png'
+folder_name2 = 'csv_original'
+folder_name3 = 'csv_resampled'
 
-subprocess.run('rclone sync ' + raspi_folder + '/png' + ' gdrive:' + gdrive_name, shell=True)
+subprocess.run('rclone sync ' + raspi_folder + '/folder_name1' + ' gdrive:' + folder_name1, shell=True)
+subprocess.run('rclone sync ' + raspi_folder + '/folder_name2' + ' gdrive:' + folder_name2, shell=True)
+subprocess.run('rclone sync ' + raspi_folder + '/folder_name3' + ' gdrive:' + folder_name3, shell=True)
+
+print('finished')
