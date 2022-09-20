28441c431e69c611256b40ebbf4889529316faf1
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-09-20 13:56:36 +0900

    change gdrive to gcs

--- Sync.py
+++ Sync.py
@@ -9,9 +9,9 @@ folder_name1 = 'png'
 folder_name2 = 'csv_original'
 folder_name3 = 'csv_resampled'
 
-subprocess.run('rclone sync ' + raspi_folder + '/' + folder_name1 + ' gdrive:' + folder_name1, shell=True)
-subprocess.run('rclone sync ' + raspi_folder + '/' + folder_name2 + ' gdrive:' + folder_name2, shell=True)
-subprocess.run('rclone sync ' + raspi_folder + '/' + folder_name3 + ' gdrive:' + folder_name3, shell=True)
+subprocess.run('rclone sync ' + raspi_folder + '/' + folder_name1 + ' gcs:' + 'practice_yasuilabo/hara/' + folder_name1, shell=True)
+subprocess.run('rclone sync ' + raspi_folder + '/' + folder_name2 + ' gcs:' + 'practice_yasuilabo/hara/' + folder_name2, shell=True)
+subprocess.run('rclone sync ' + raspi_folder + '/' + folder_name3 + ' gcs:' + 'practice_yasuilabo/hara/' + folder_name3, shell=True)
 
 print('finished')
 
